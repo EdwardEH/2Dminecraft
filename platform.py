bcb2f26cfd1492ab3ae14d937e9c684c05f4d6de
Edward <edwardehaughton@gmail.com>
2021-02-07 15:55:41 +0000

    Now has coulor and nastys

--- platform.py
+++ platform.py
@@ -3,6 +3,43 @@ import random
 import os
 import time
 
+class sprite:
+   def __init__(self,hostile,drops,t,x,y):
+      self.h=hostile
+      self.drops=drops
+      self.x=x
+      self.y=y
+      self.Type=t
+
+   def move(self):
+      global end,lives
+      di=random.choice(['up','down','left','right'])
+      if di == 'up':
+         board[self.y][self.x]=' '
+         self.y-=1
+         board[self.y][self.x]=self.Type
+      elif (di == 'down') and (not board[self.y+1][self.x]=='#'):
+         board[self.y][self.x]=' '
+         self.y+=1
+         board[self.y][self.x]=self.Type
+      elif (di == 'left') and (not board[self.y][self.x+1] == '#'):
+         board[self.y][self.x]=' '
+         self.x+=1
+         board[self.y][self.x]=self.Type
+      elif (di == 'right') and (not board[self.y][self.x-1] == '#'):
+         board[self.y][self.x]=' '
+         self.x-=1
+         board[self.y][self.x]=self.Type
+      if (self.h==True) and ( board[self.x][self.x]==pl.player):
+         if lives==1:
+            print('you died')
+            end = True
+         else:
+            lives-=1
+      else:
+         upboard()
+             
+
 class block:
    def __init__(self,Type,x,y):
       self.Type=Type
@@ -16,18 +53,19 @@ class item:
       self.name=n
 
    def craft(self):
-      global t
-      os.system('clear')
-      name = input('>')
-      if name == self.name:
-         v=0
-         for i in self.crafting:
-            pl.inv[self.crafting[v]]-=1
-            v+=1
-         self.got = True
-         pl.inv[self.name]=1
-         if self.name == 'pickaxe':
-            t-=2
+      global t,lives
+      v=0
+      for i in self.crafting:
+         pl.inv[self.crafting[v]]-=1
+         v+=1
+      self.got = True
+      pl.inv[self.name]=1
+      if self.name == 'matock':
+         t-=2
+      elif self.name == 'sapling':
+         screen.trees(1)
+      elif self.name == 'armour':
+         lives+=1
 
 class screen:
    def __init__(self,width,height):
@@ -90,7 +128,7 @@ class players:
    def __init__(self,x,y,p):
       self.x = x
       self.y = y
-      self.inv = {'wood':0,'stone':0,'iron':0}
+      self.inv = {'wood':0,'stone':0,'iron':0,'warg hide':0}
       self.player = p
 
    def move(self,di):
@@ -127,6 +165,15 @@ class players:
             self.inv['stone']-=1
          else:
             print('you have run out of stone')
+      if (di == 'a') and (spear.got):
+         board[self.y][self.x]=' '
+         self.x+=1
+         board[self.y][self.x]=self.player
+         if board[self.y][self.x]=='b':
+            for mob in mobs:
+               if board[mob.y][mob.x]==self.player:
+                  for i in mob.drops:
+                     self.inv[i]+=1
       upboard()
 
    def inventry(self):
@@ -141,30 +188,55 @@ class players:
          board[self.y][self.x]=self.player
 
 def upboard():
-   os.system('clear')
+   print('\33[H')
    for i in board:
       for r in i:
-         print(r,end='')
+         print(coulors[r]+r+coulors['end'],end='')
       print()
 
+def mob_pos():
+   while True:
+      x=random.randint(0,39)
+      y=random.randint(0,39)
+      if board[y][x]==' ':
+         return x,y
+
+print('\33[2J')
 t=2
+end=False
+lives=1
 
-pickaxe=item(False,['wood','iron'],'pickaxe')
+coulors={'#':'\33[100m','b':'\33[92m','s':'\33[94m','^':'\33[38;5;94m','end':'\33[0m',' ':'\33[0m'}
 
-items = ['wood','stone','iron']
+pickaxe=item(False,['wood','iron'],'matock')
+sapling=item(False,['wood'],'sapling')
+spear=item(False,['wood','wood','iron','iron'],'spear')
+armour=item(False,['warg hide'],'armour')
+
+items = {'matock':pickaxe,'sapling':sapling,'spear':spear,'armour':armour}
+mobs=[]
 
 screen=screen(40,40)
 board = screen.new_board()
 screen.terrain()
 screen.trees(random.randint(1,5))
 pl=players(0,19,'s')
-while True:
+
+while not end:
    a = keys.read()
    if a == 'e':
       pl.inventry()
    elif a == 'c':
-      pickaxe.craft()
+      os.system('clear')
+      n=input('>')
+      items[n].craft()
+   elif random.choice([True,False]) and len(mobs)<3:
+      x,y=mob_pos()
+      mob = sprite(True,['iron','warg hide'],'b',x,y)
+      mobs+=[mob]
    else:
+      for mob in mobs:
+         mob.move()
       pl.move(a)
       pl.do_gravity()
 
